3dbacf4af1c20703ed3030438412b5353743e3f1
marloncarvalho <marlon.carvalho@gmail.com>
2014-01-27 11:22:58 -0300

    Improving Options Pages

--- js/options.js
+++ js/options.js
@@ -1,6 +1,13 @@
 $("#save").on("click", function save_options() {
 	localStorage["semantria_key"] = $("#key").val();
 	localStorage["semantria_secret"] =  $("#secret").val();
+	$("#success-panel").show();
+});
+
+$("#close").on("click", function save_options() {
+	chrome.tabs.getCurrent(function(tab) {
+	    chrome.tabs.remove(tab.id, function() { });
+	});
 });
 
 document.addEventListener('DOMContentLoaded', function restore_options() {
